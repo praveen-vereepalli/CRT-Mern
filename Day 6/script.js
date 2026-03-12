86412eb1a2e8f41be8bce4f22e7490b2330952a6
praveen-vereepalli <praveenveerepalli7@gmail.com>
2026-03-12 12:20:29 +0530

    Second commit

--- Day 6/script.js
+++ Day 6/script.js
@@ -5,10 +5,13 @@ const searchBox = document.querySelector('.input');
 const serachBtn = document.querySelector('button');
 
 async function checkwheather(city) {
-    var response = await fetch(apiUrl+ '&appid='+ apikey+'&q='+city);
-    var data= await response.json();
+    var response =await fetch(apiUrl+ '&appid='+ apikey+'&q='+city);
+    var data=await response.json();
     console.log(data);
-    document.querySelector('.city').innerHTMl=data.name
+    document.querySelector('.city').innerHTML=data.name
     document.querySelector('.temperature').innerHTML=Math.round(data.main.temp)+'°C';
+    document.querySelector('.humidityValue').innerHTML=data.main.humidity+'%';
+    document.querySelector('.speedValue').innerHTML=data.wind.speed+'km/h';
+
     
 }
